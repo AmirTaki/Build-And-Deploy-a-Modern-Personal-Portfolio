375c3b7a42ff0fa4de9d45d29cd3174bd69a0f1d
amir <amirtaki623@gmail.com>
2025-07-26 22:51:57 +0330

    ad utils at component theme toggle at proejct

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,6 @@
 import { Moon, Sun } from "lucide-react"
 import { useEffect, useState } from "react"
+import { cn } from "../lib/utils"
 
 
 export const ThemeToggle = () =>{
@@ -15,7 +16,7 @@ export const ThemeToggle = () =>{
            setIsDarkMode(false)
        }
     },[])
-    
+
     const toggleTheme = () => {
     //   setIsDarkMode(isDarkMode ? false : true)
     if(isDarkMode){
@@ -33,7 +34,9 @@ export const ThemeToggle = () =>{
     }
     return(
         <>
-          <button onClick={toggleTheme}>
+          <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300 ",
+            "focus:outline-hidden"
+          )}>
             {isDarkMode ? (
                 <Sun className="h-6 w-6 text-yellow-300" /> ):( 
                 <Moon className="h-6 w-6 text-blue-900"/>
