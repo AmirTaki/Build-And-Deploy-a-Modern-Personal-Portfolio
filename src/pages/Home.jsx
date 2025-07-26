33c1a852b3ad6c2f9e33bfd8df493b262451963d
amir <amirtaki623@gmail.com>
2025-07-26 22:26:11 +0330

    add ThemeToggle components at project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 const Home = () => {
     return(
-        <div className="min-h-screen ">
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden ">
             {/* Theme Toggle */}
+            
             {/* Background Effects */}
             {/* Navbar */}
             {/* Main Content */}
