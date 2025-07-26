72ef4c95999bf35d71cbb557f9559fa6b428078c
amir <amirtaki623@gmail.com>
2025-07-26 23:07:18 +0330

    add useEffects at project

--- src/components/StartBckground.jsx
+++ src/components/StartBckground.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const StartBackground = () => {
     const [stars, setStars] = useState([])
-
+    useEffect(()=>{
+        generateStarts()
+    },[])
     const generateStarts = () =>{
         const numberOfStars = Math.floor(window.innerWidth * window.innerHeight / 10000)
         const newStars = []
@@ -14,9 +16,10 @@ const StartBackground = () => {
                 y: Math.random() * 100,
                 opacity : Math.random() * .5 + .5,
                 animationDuration : Math.random() * 4 + 2,
-                
             })
         }
+
+        setStars(newStars)
         
     }
     return(
