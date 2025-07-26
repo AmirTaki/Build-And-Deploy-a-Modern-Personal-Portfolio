3b25dfebf0d5b38a904110028038b49f006f29ec
amir <amirtaki623@gmail.com>
2025-07-26 23:02:35 +0330

    generateStarts functions

--- src/components/StartBckground.jsx
+++ src/components/StartBckground.jsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
 const StartBackground = () => {
+    const [stars, setStars] = useState([])
+
+    const generateStarts = () =>{
+        const numberOfStars = Math.floor(window.innerWidth * window.innerHeight / 10000)
+        const newStars = []
+
+        
+    }
     return(
         <>
         
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
+import StartBackground from "../components/StartBckground";
 import { ThemeToggle } from "../components/ThemeToggle";
-
 const Home = () => {
     return(
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden ">
             {/* Theme Toggle */}
             <ThemeToggle />
             {/* Background Effects */}
+            <StartBackground />
+
             {/* Navbar */}
             {/* Main Content */}
             {/* Footer */}
