7931eb4a0dd13ad8949042dc4ec62883b54c5c0a
amir <amirtaki623@gmail.com>
2025-07-26 23:05:49 +0330

    generateStarts functions part 2

--- src/components/StartBckground.jsx
+++ src/components/StartBckground.jsx
@@ -6,7 +6,17 @@ const StartBackground = () => {
     const generateStarts = () =>{
         const numberOfStars = Math.floor(window.innerWidth * window.innerHeight / 10000)
         const newStars = []
-
+        for(let i = 0; i < numberOfStars ; i ++){
+            newStars.push({
+                id:i,
+                size : Math.random() * 3 + 1,
+                x : Math.random() * 100,
+                y: Math.random() * 100,
+                opacity : Math.random() * .5 + .5,
+                animationDuration : Math.random() * 4 + 2,
+                
+            })
+        }
         
     }
     return(
