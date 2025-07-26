5af098c8fd999793a5386021e6973e391c04da53
amir <amirtaki623@gmail.com>
2025-07-26 22:27:51 +0330

    add use State at theme toggle at project

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+export const ThemeToggle = () =>{
+    const[isDarkMode, setIsDarkMode] = useState(false)
+    return(
+        <>
+          <button></button>
+        </>
+    )
+}
