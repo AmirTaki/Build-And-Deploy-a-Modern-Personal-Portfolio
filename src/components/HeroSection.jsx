c1f1108977aca137e591970546181b710de16b95
amir <amirtaki623@gmail.com>
2025-07-27 21:45:16 +0330

    Hero Section Implementation part 3

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,9 +7,9 @@ const HeroSection = () => {
             <div className="container max-w-4xl mx-auto text-center z-10">
                 <div className="space-y-6">
                     <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
-                        <span> Hi, I'm</span>
-                        <span> Pedro</span>
-                        <span> Machado</span>
+                        <span className="opacity-0 animate-fade-in "> Hi, I'm</span>
+                        <span className="text-primary opacity-0 animate-fade-in-delay-1"> {" "} Pedro</span>
+                        <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2"> Machado</span>
                     </h1>
                 </div>
             </div>
