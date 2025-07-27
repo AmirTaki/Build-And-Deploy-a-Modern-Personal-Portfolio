951929b95d9e154e634c95ba932d2f7ee1b0acf4
amir <amirtaki623@gmail.com>
2025-07-27 23:02:17 +0330

    About Me Section part 5

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,9 +3,21 @@ const AboutSection = () =>{
        <section id = "about" className="py-24 px-4 relative">
         {" "}
         <div className="container mx-auto max-w-5xl"></div>
-            <h2>
+            <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
                 About <span className="text-primary">Me</span>
             </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+                <div className="space-y-6">
+                    <h3>Passionate Web Developer & Tech Creator</h3>
+                    <p>
+                        With over 5 years of experience in web development, I specialize
+                        in creating responsive, accessible, and performant web applications using modern technologies.
+                    </p>
+                </div>
+                <div className="">
+
+                </div>
+            </div>
        </section>
     )
 }
