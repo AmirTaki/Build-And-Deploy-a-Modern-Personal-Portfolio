71536c8ed6f349e0c7ddb53da9a51f3f69057996
amir <amirtaki623@gmail.com>
2025-07-27 21:42:15 +0330

    Hero Section Implementation part 2

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,8 +5,12 @@ const HeroSection = () => {
             className="relative min-h-screen flex flex-col items-center justify-center px-4"
         >
             <div className="container max-w-4xl mx-auto text-center z-10">
-                <div className="">
-                    
+                <div className="space-y-6">
+                    <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+                        <span> Hi, I'm</span>
+                        <span> Pedro</span>
+                        <span> Machado</span>
+                    </h1>
                 </div>
             </div>
         </section>
