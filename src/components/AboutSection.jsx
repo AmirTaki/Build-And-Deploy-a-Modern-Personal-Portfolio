fba00da8c5a34ac62b7d01cf8eb4286ae68cc35c
amir <amirtaki623@gmail.com>
2025-07-27 23:27:18 +0330

    About Me Section part 13

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,4 +1,4 @@
-import { Code } from "lucide-react"
+import { Briefcase, Code, User } from "lucide-react"
 
 const AboutSection = () =>{
     return(
@@ -53,14 +53,14 @@ const AboutSection = () =>{
                         <div className="gradient-border p-6 card-hover">
                             <div className="flex items-start gap-4">
                                 <div className="p-3 rounded-full bg-primary/10">
-                                    <Code className="h-6 w-6 text-primary"/>
+                                    <User className="h-6 w-6 text-primary"/>
                                 </div>
                             </div>
                         </div>
                         <div className="gradient-border p-6 card-hover">
                             <div className="flex items-start gap-4">
                                 <div className="p-3 rounded-full bg-primary/10">
-                                    <Code className="h-6 w-6 text-primary"/>
+                                    <Briefcase className="h-6 w-6 text-primary"/>
                                 </div>
                             </div>
                         </div>
