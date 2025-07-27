5e0bcc9cb750d641337f7b08b422b8350dfe0e3e
amir <amirtaki623@gmail.com>
2025-07-27 23:38:31 +0330

    add Skils at Skills Section at project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import StartBackground from "@/components/StartBckground";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
+import SkillsSection from "@/components/SkillsSection";
+
 const Home = () => {
     return(
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden ">
@@ -16,6 +18,7 @@ const Home = () => {
             <main>
                 <HeroSection />
                 <AboutSection/>
+                <SkillsSection />
             </main>
             {/* Footer */}
         </div>
