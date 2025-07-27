191473c62d9fd517e38eb6d7aba3a3c7dc6e9d38
amir <amirtaki623@gmail.com>
2025-07-27 23:46:57 +0330

    add Skills Section part 6

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -25,8 +25,13 @@ const SkillsSection = () => {
         <section id = "skills" className="py-24 px-4 relative bg-secondary/30">
             <div className="container mx-auto max-w-5xl">
                 <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
-                    My <span> Skills</span>
+                    My <span className="text-primary"> Skills</span>
                 </h2>
+                <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {skills.map((skill, key) =>(
+                        <div className="bg-card p-6 rounded-lg shadow-xs card-hover" key = {key}></div>
+                    ))}
+                </div>
             </div>
         </section>
     )
