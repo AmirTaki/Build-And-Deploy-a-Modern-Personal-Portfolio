5a042c513d0e3b816cf12944d378a64f5f87459b
amir <amirtaki623@gmail.com>
2025-07-27 21:25:37 +0330

    | Building the Navbar part 8

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,8 @@ const navItems = [
 ]
 const Navbar = () => {
     const[isScrolled, setIsScrolled] = useState(false)
+    const[isMenuOpen, setIsMenuOpen] = useState(true)
+
     useEffect(()=>{
         const handleScroll = () =>{
             setIsScrolled(window.screenY > 10)
@@ -38,9 +40,9 @@ const Navbar = () => {
                 {/* mobile nav */}
                 <div className={cn("fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center",
                     "transition-all duration-300 md:hidden",
-                    
+                    isMenuOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
                 )}>
-                    <div className="hidden md:flex space-x-8">
+                    <div className="flex flex-col space-y-8 text-xl">
                         {navItems.map((item, key) => (
                             <a key = {key} herf = {item.href} className="text-foreground/80 hover:text-primary transition-colors duration-300">
                                 {item.name}
