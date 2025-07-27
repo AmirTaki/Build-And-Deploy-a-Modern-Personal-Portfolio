eaa548afb1378560d32b74a11deae0c5ac7f3610
amir <amirtaki623@gmail.com>
2025-07-27 23:51:54 +0330

    add Skills Section part 8

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -33,6 +33,11 @@ const SkillsSection = () => {
                             <div className="text-left mb-4">
                                 <h3 className="font-semibold text-lg">{skill.name}</h3>
                             </div>
+                            <div className="w-full bg-secodary/50 h-2 rounded-full overflow-hidden">
+                                <div className="bg-primary h-2 rounded-full origin-left animate-[grow_1.5s_ease-out]">
+
+                                </div>
+                            </div>
                         </div>
                     ))}
                 </div>
