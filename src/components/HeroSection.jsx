6938b05b6f9be2b39972dff35a36eca28fe1aa41
amir <amirtaki623@gmail.com>
2025-07-27 21:39:46 +0330

    Hero Section Implementation part 1

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,8 +1,15 @@
 const HeroSection = () => {
     return (
-        <>
-        test
-        </>
+        <section 
+            id = "hero"
+            className="relative min-h-screen flex flex-col items-center justify-center px-4"
+        >
+            <div className="container max-w-4xl mx-auto text-center z-10">
+                <div className="">
+                    
+                </div>
+            </div>
+        </section>
     )
 }
 export default HeroSection;
