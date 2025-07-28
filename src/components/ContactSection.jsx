aab95864fbf325cac71a3626ec7625c06d26b2df
amir <amirtaki623@gmail.com>
2025-07-28 23:12:45 +0330

    Conatct Section part 4

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,7 +1,9 @@
 const ContactSection = () => {
     return(
         <section id = "contact" className="py-24 px-4 relative bg-secondary/30">
-
+            <h2>
+                Get In <span className="text-primary"> Touch</span>
+            </h2>
         </section>
     )
 }
