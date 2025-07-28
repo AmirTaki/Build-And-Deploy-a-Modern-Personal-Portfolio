7f537896540b68744b624851bc1db194673b970a
amir <amirtaki623@gmail.com>
2025-07-28 23:31:05 +0330

    Conatct Section part 14

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -52,16 +52,16 @@ const ContactSection = () => {
                         <div className="pt-8">
                             <h4 className="font-medium mb-4"> Connect With Me</h4>
                             <div className="flex space-x-4 justify-center">
-                                <a href="">
+                                <a href="#" target="_blank">
                                     <Linkedin />
                                 </a>
-                                <a href="">
+                                <a href="#" target="_blank">
                                     <Twitter />
                                 </a>
-                                <a href="">
+                                <a href="#" target="_blank">
                                     <Instagram />
                                 </a>
-                                <a href="">
+                                <a href="#" target="_blank">
                                     <Twitch />
                                 </a>
                             </div>
