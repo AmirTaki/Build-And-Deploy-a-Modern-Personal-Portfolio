5afc8a7ea01cd034cfb450eb2637ccdfba1a34b6
amir <amirtaki623@gmail.com>
2025-07-28 22:30:47 +0330

    project section part 11

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -36,7 +36,11 @@ const ProjectsSection = () =>{
     return(
       <section id = "projects" className="py-24 px-4 relative">
         <div className="container mx-auto max-w-5xl">
-            <h2>Featured <span className="text-primary">Projects</span></h2>
+            <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center ">Featured <span className="text-primary">Projects</span></h2>
+            <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+                Here are some of my recent projects. Each project was carefully
+                crafted with attention to detail, performance, and user experience.
+            </p>
         </div>
       </section>
     )
