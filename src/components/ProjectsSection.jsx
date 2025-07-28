6b51dee19e6f55782de6e5691d7e706bc85a5199
amir <amirtaki623@gmail.com>
2025-07-28 22:33:44 +0330

    project section part 13

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -42,7 +42,11 @@ const ProjectsSection = () =>{
                 crafted with attention to detail, performance, and user experience.
             </p>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                {}
+                {projects.map((project, key) => {
+                    <div key = {key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
+                        <div className=""></div>
+                    </div>
+                })}
             </div>
         </div>
       </section>
