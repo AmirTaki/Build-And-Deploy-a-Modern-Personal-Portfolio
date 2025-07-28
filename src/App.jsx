9e021ef17ad0c82e21057df9d0f772893b764210
amir <amirtaki623@gmail.com>
2025-07-28 23:55:48 +0330

    ui at project

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
+import {Toaster} from "./components/ui/Toaster"
 
 function App() {
 
 
   return (
     <>
+    <Toaster />
       <BrowserRouter>
         <Routes>
           <Route index  element = {<Home />} />
