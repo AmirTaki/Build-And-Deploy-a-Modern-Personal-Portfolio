b33ccd088a6771b99ab12f92b5245d53be61399b
amir <amirtaki623@gmail.com>
2025-07-29 22:41:58 +0330

    footer part 4

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,10 @@ const Footer = () => {
     return (
         <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8  flex flex-wrap justify-between items-center">
             {" "}
-            <p>&copy; {new Date().getFullYear()} Amir.Taki. All rights reserved.</p>
+            <p className="text-sm tex-muted-foreground">
+                {" "}
+                &copy; {new Date().getFullYear()} Amir.Taki. All rights reserved.
+            </p>
         </footer>
     )
 }
