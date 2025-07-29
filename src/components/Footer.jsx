b7bb1c7816c93ef40cef7ac9c4c715dee045c16a
amir <amirtaki623@gmail.com>
2025-07-29 22:44:17 +0330

    footer part 4

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { ArrowDown, ArrowUp } from "lucide-react";
+
 const Footer = () => {
     return (
         <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8  flex flex-wrap justify-between items-center">
@@ -6,6 +8,9 @@ const Footer = () => {
                 {" "}
                 &copy; {new Date().getFullYear()} Amir.Taki. All rights reserved.
             </p>
+            <a href="#hero" className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 ">
+                <ArrowUp />
+            </a>
         </footer>
     )
 }
